5d3743d05f1ecbebb42982c90494b525b914bea4
thiagoassisk8 <tyagosilva82@gmail.com>
2021-12-02 16:23:38 -0300

    atualizacoes

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -30,7 +30,7 @@ class _HomeState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: Text('Pokedx'),
+        title: Text('Pokedex'),
       ),
       body: (_isLoading)
           ? Center(
